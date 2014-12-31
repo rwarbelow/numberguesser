478e4eb26ced3e7135bc614d67884141b8f72fc4
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-12-30 17:36:24 -0700

    initial commit

---
+++ lib/runner.rb
@@ -0,0 +1,3 @@
+require_relative 'printer'
+require_relative 'evaluator'
+
---
+++ test/evaluator_test.rb
@@ -0,0 +1,10 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/evaluator'
+
+class EvaluatorTest < Minitest::Test
+	def test_it_does_something
+		skip
+	end
+end
